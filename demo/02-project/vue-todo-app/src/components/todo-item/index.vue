c3804ca893d5753cfd622caf99af201c4b7fb0f3
hmiinyu <hmiinyu@gmail.com>
2019-08-27 22:14:39 +0800

    feat(): update todo-item style

--- demo/02-project/vue-todo-app/src/components/todo-item/index.vue
+++ demo/02-project/vue-todo-app/src/components/todo-item/index.vue
@@ -38,42 +38,34 @@
     padding: 0 5px;
     border-bottom: 1px solid #ddd;
   }
-
   li .btn {
     display: none;
   }
-
   li.hover {
     cursor: pointer;
     background: #ddd;
   }
-
   li.hover .btn {
     display: block;
   }
-
   li label {
     float: left;
     cursor: pointer;
   }
-
   li label li input {
     vertical-align: middle;
     margin-right: 6px;
     position: relative;
     top: -1px;
   }
-
   li button {
     float: right;
     display: none;
     margin-top: 3px;
   }
-
   li:before {
     content: initial;
   }
-
   li:last-child {
     border-bottom: none;
   }
