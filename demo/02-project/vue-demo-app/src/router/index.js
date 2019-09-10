b50a2c3e3b81deed0eb5512dc7ffc8e7b1d73aef
hmiinyu <hmiinyu@gmail.com>
2019-09-10 16:31:00 +0800

    feat(): add vue demo 1.2

--- demo/02-project/vue-demo-app/src/router/index.js
+++ demo/02-project/vue-demo-app/src/router/index.js
@@ -7,6 +7,7 @@ Vue.use(VueRouter)
 export default new VueRouter({
   routes: [
     { path: '/', name: '主页', component: Home },
-    { path: '/1.1', name: 'Vue三大核心概念(属性、事件、插槽)', component: () => import('@/views/1.1') }
+    { path: '/1.1', name: 'Vue三大核心概念(属性、事件、插槽)', component: () => import('@/views/1.1') },
+    { path: '/1.2', name: '双向绑定和单向数据流不冲突', component: () => import('@/views/1.2') }
   ]
 })
